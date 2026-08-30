4fbb81dedf1398c14b55cf0f7e51f2337d883069
Shaik Arshia <shaikarshia49@gmail.com>
2026-08-30 11:26:12 +0530

    Fix authentication flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+
 import Login from './components/Login'
 import Home from './components/Home'
 import Popular from './components/Popular'
@@ -9,66 +10,76 @@ import Account from './components/Account'
 import ProtectedRoute from './components/ProtectedRoute'
 import Header from './components/Header'
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      {/* Public Route */}
-      <Route path="/login" element={<Login />} />
+const App = () => {
+  // Remove old login session whenever the app starts
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/login" element={<Login />} />
 
-      {/* Protected Routes */}
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Header />
-            <Home />
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected Home Route */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Header />
+              <Home />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/popular"
-        element={
-          <ProtectedRoute>
-            <Header />
-            <Popular />
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected Popular Route */}
+        <Route
+          path="/popular"
+          element={
+            <ProtectedRoute>
+              <Header />
+              <Popular />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/search"
-        element={
-          <ProtectedRoute>
-            <Header />
-            <Search />
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected Search Route */}
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <Header />
+              <Search />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/movies/:id"
-        element={
-          <ProtectedRoute>
-            <Header />
-            <MovieDetails />
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected Movie Details Route */}
+        <Route
+          path="/movies/:id"
+          element={
+            <ProtectedRoute>
+              <Header />
+              <MovieDetails />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/account"
-        element={
-          <ProtectedRoute>
-            <Header />
-            <Account />
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected Account Route */}
+        <Route
+          path="/account"
+          element={
+            <ProtectedRoute>
+              <Header />
+              <Account />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
-  </BrowserRouter>
-)
+        {/* Not Found Route */}
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
 
 export default App
